65f4617b78004c62cd3ab043bd310cb09d07c88a
Oleksandr Kolomoiets <redfoxius@gmail.com>
2025-06-03 23:47:15 +0300

    init

--- main.go
+++ main.go
@@ -28,8 +28,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	ips := generateIPs(*maskPtr, *numPtr)
 	for i := 0; i < *numPtr; i++ {
-		if _, err := f.Write([]byte(generateIP(*maskPtr) + "\n")); err != nil {
+		if _, err := f.Write([]byte(ips[i])); err != nil {
 			log.Fatal(err)
 		}
 	}
@@ -42,28 +44,40 @@ func main() {
 	log.Printf("Generation took %s", elapsed)
 }
 
-func generateIP(mask string) string {
+func generateIPs(mask string, num int) []string {
 	elems := strings.Split(mask, ".")
+	result := make([]string, num)
+	if validateMask(elems) {
+		for i := 0; i < num; i++ {
+			res := append(elems[:0:0], elems...)
+
+			for j := 0; j < 4; j++ {
+				if res[j] == "*" {
+					res[j] = strconv.Itoa(randRange(0, 255))
+				}
+			}
+
+			result[i] = strings.Join(res, ".") + "\n"
+		}
+	}
+
+	return result
+}
+
+func randRange(min, max int) int {
+	return rand.IntN(max+1-min) + min
+}
+
+func validateMask(elems []string) bool {
 	if len(elems) != 4 {
 		log.Fatal("mask length is not 4")
 	}
-
-	result := make([]string, 4)
 	for i := 0; i < 4; i++ {
 		if elems[i] != "*" {
 			if n, err := strconv.Atoi(elems[i]); err != nil || n > 255 || n < 0 {
 				log.Fatal("mask is not valid")
 			}
-
-			result[i] = elems[i]
-			continue
 		}
-		result[i] = strconv.Itoa(randRange(0, 255))
 	}
-
-	return strings.Join(result, ".")
-}
-
-func randRange(min, max int) int {
-	return rand.IntN(max+1-min) + min
+	return true
 }
